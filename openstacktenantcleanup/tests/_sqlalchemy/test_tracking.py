35127fde7119c5befcc8110c2cd393d0dcf24830
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-04-27 12:47:23 +0100

    Work on fixing tests.

--- openstacktenantcleanup/tests/_sqlalchemy/test_tracking.py
+++ openstacktenantcleanup/tests/_sqlalchemy/test_tracking.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 from openstacktenantcleanup._sqlalchemy.tracking import SqlTracker
 from openstacktenantcleanup.external.sequencescape.stub_database import create_stub_database
-from openstacktenantcleanup.models import OpenstackKeyPair, OpenstackImage, OpenstackInstance
+from openstacktenantcleanup.models import OpenstackKeypair, OpenstackImage, OpenstackInstance
 
 
 class TestSqlTracker(unittest.TestCase):
@@ -15,24 +15,24 @@ class TestSqlTracker(unittest.TestCase):
         self.tracker = SqlTracker(f"{dialect}:///{database_location}")
 
     def test_get_age_when_does_not_exist(self):
-        item = OpenstackKeyPair(identifier=123)
+        item = OpenstackKeypair(identifier="123")
         self.assertIsNone(self.tracker.get_age(item))
 
     def test_get_registered_identifiers(self):
-        items = [OpenstackKeyPair(identifier=123), OpenstackImage(identifier=456), OpenstackInstance(identifier=789)]
+        items = [OpenstackKeypair(identifier="1"), OpenstackImage(identifier="2"), OpenstackInstance(identifier="3")]
         self.tracker.register(items)
         registered = self.tracker.get_registered_identifiers()
         self.assertEquals(set(registered), {item.identifier for item in items})
 
     def test_get_registered_identifiers_of_type(self):
-        test_type = OpenstackKeyPair
-        items = {test_type(identifier=i) for i in range(10)}
+        test_type = OpenstackKeypair
+        items = {test_type(identifier=str(i)) for i in range(10)}
         self.tracker.register(items)
         registered = self.tracker.get_registered_identifiers(item_type=test_type)
         self.assertEquals(set(registered), {item.identifier for item in items})
 
     def test_register_with_created_time(self):
-        item = OpenstackImage(identifier=123, created_at=datetime(2016, 1, 1))
+        item = OpenstackImage(identifier="123", created_at=datetime(2016, 1, 1))
         start_time = datetime.now()
         self.tracker.register(item)
         age = self.tracker.get_age(item)
@@ -45,7 +45,7 @@ class TestSqlTracker(unittest.TestCase):
         self.assertLessEqual(age, maximum_age)
 
     def test_register_without_created_time(self):
-        item = OpenstackKeyPair(identifier=123)
+        item = OpenstackKeypair(identifier="123")
         start_time = datetime.now()
         self.tracker.register(item)
         age = self.tracker.get_age(item)
@@ -57,16 +57,16 @@ class TestSqlTracker(unittest.TestCase):
         self.assertLessEqual(age, maximum_age)
 
     def test_register_when_already_registered(self):
-        item = OpenstackKeyPair(identifier=123)
+        item = OpenstackKeypair(identifier="123")
         self.tracker.register(item)
         self.tracker.register(item)
 
     def test_unregister_when_not_exists(self):
-        item = OpenstackKeyPair(identifier=123)
+        item = OpenstackKeypair(identifier="123")
         self.tracker.unregister(item)
 
     def test_unregister(self):
-        item = OpenstackKeyPair(identifier=123)
+        item = OpenstackKeypair(identifier="123")
         self.tracker.register(item)
         self.tracker.unregister(item)
         self.assertIsNone(self.tracker.get_age(item))
--- openstacktenantcleanup/tests/test_configuration.py
+++ openstacktenantcleanup/tests/test_configuration.py
@@ -1,58 +1,55 @@
 import os
-import re
 import unittest
 from datetime import timedelta
 from logging import getLevelName
 
-from openstacktenantcleanup.configuration import parse_configuration, Configuration, GeneralConfiguration, \
-    LogConfiguration, CleanupConfiguration, CleanupAreaConfiguration
+from openstacktenantcleanup.configuration import parse_configuration, GeneralConfiguration, LogConfiguration
+from openstacktenantcleanup.managers import OpenstackInstanceManager, OpenstackKeypairManager, OpenstackImageManager
 from openstacktenantcleanup.models import OpenstackCredentials
 
 _RESOURCE_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources")
 
 _EXAMPLE_VALID_CONFIGURATION_LOCATION = os.path.join(_RESOURCE_DIRECTORY, "valid.config.yml")
-_EXAMPLE_VALID_CONFIGURATION = Configuration(
-    general_configuration=GeneralConfiguration(
-        run_period=timedelta(hours=1),
-        log=LogConfiguration(
-            location="/my-log",
-            level=getLevelName("WARN")
-        )
-    ),
-    cleanup_configurations=[
-        CleanupConfiguration(
-            credentials=[OpenstackCredentials(
-                auth_url="http://example.com:5000/v2.0",
-                tenant="my-tenant",
-                username="my-username",
-                password="my-password"
-            )],
-            instance_cleanup_configuration=CleanupAreaConfiguration(
-                remove_if_older_than=timedelta(days=1),
-                excludes=[re.compile("my-special-instance.*")]
-            ),
-            image_cleanup_configuration=CleanupAreaConfiguration(
-                remove_only_if_unused=True,
-                remove_if_older_than=timedelta(days=31),
-                excludes=[re.compile("my-special-image[0-9]+")]
-            ),
-            keypair_cleanup_configuration = CleanupAreaConfiguration(
-                remove_only_if_unused=True,
-                remove_if_older_than=timedelta(hours=1),
-                excludes=[re.compile("my-special-key-pair")]
-            )
-        )
-    ]
+_EXAMPLE_VALID_GENERAL_CONFIGURATION = GeneralConfiguration(
+    run_period=timedelta(hours=1),
+    log=LogConfiguration(
+        location="/my-log",
+        level=getLevelName("WARN")
+    )
 )
+_EXAMPLE_VALID_CREDENTIALS = [OpenstackCredentials(
+    auth_url="http://example.com:5000/v2.0/",
+    tenant="my-tenant",
+    username="my-username",
+    password="my-password"
+)]
 
 
 class TestParseConfiguration(unittest.TestCase):
     """
     Tests for `parse_configuration`.
     """
-    def test_with_full_config(self):
+    def test_parse_valid_configuration(self):
         configuration = parse_configuration(_EXAMPLE_VALID_CONFIGURATION_LOCATION)
-        self.assertEquals(_EXAMPLE_VALID_CONFIGURATION, configuration)
+
+        self.assertEquals(_EXAMPLE_VALID_GENERAL_CONFIGURATION, configuration.general_configuration)
+        self.assertEquals(1, len(configuration.cleanup_configurations))
+
+        cleanup_configuration = configuration.cleanup_configurations[0]
+        self.assertEquals(_EXAMPLE_VALID_CREDENTIALS, cleanup_configuration.credentials)
+
+        cleanup_areas = cleanup_configuration.cleanup_areas
+        self.assertEquals(3, len(cleanup_areas))
+
+        instance_prevent_delete_detectors = cleanup_areas[OpenstackInstanceManager]
+        self.assertEquals(2, len(instance_prevent_delete_detectors))
+
+        image_prevent_delete_detectors = cleanup_areas[OpenstackImageManager]
+        self.assertEquals(4, len(image_prevent_delete_detectors))
+
+        keypair_prevent_delete_detectors = cleanup_areas[OpenstackKeypairManager]
+        self.assertEquals(3, len(keypair_prevent_delete_detectors))
+
 
 
 if __name__ == "__main__":
