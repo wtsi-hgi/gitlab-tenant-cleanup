6bbe4c7753bec115a8fcc61bc53c173df4336b28
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-04-27 14:37:00 +0100

    Completes planning functionality.

--- openstacktenantcleanup/planning.py
+++ openstacktenantcleanup/planning.py
@@ -1,3 +1,6 @@
+import logging
+from concurrent.futures import ThreadPoolExecutor
+
 from typing import List, Iterable, Tuple, Collection, Callable, Type, Dict
 
 from openstacktenantcleanup.common import create_human_identifier
@@ -9,11 +12,13 @@ from openstacktenantcleanup.tracking import Tracker
 
 ItemAndReasons = Tuple[OpenstackItem, Collection[str]]
 DeleteSetup = Tuple[OpenstackItem, Callable[[OpenstackItem], None]]
-CleanUpProposal = List[Dict[Type[Manager],
-                            Tuple[Collection[DeleteSetup], Collection[ItemAndReasons], Collection[ItemAndReasons]]]]
+CleanUpPlans = List[Dict[Type[Manager],
+                         Tuple[Collection[DeleteSetup], Collection[ItemAndReasons], Collection[ItemAndReasons]]]]
+
+_logger = logging.getLogger(__name__)
 
 
-def create_clean_up_proposals(configuration: Configuration, tracker: Tracker, dry_run: bool=True) -> CleanUpProposal:
+def create_clean_up_plans(configuration: Configuration, tracker: Tracker, dry_run: bool=True) -> CleanUpPlans:
     """
     TODO
     :param configuration: 
@@ -21,11 +26,11 @@ def create_clean_up_proposals(configuration: Configuration, tracker: Tracker, dr
     :param dry_run: 
     :return: 
     """
-    cleaup_proposal: CleanUpProposal = []
+    cleaup_plans: CleanUpPlans = []
 
     # TODO: cleanup in order: instances first, others next
     for cleanup_configuration in configuration.cleanup_configurations:
-        cleanup_areas_proposals = {}
+        cleanup_areas_plans = {}
 
         for manager_type, prevent_delete_detectors in cleanup_configuration.cleanup_areas.items():
             # Need to use all credentials when cleaning up keys, as they can only be removed by the account that created
@@ -44,29 +49,48 @@ def create_clean_up_proposals(configuration: Configuration, tracker: Tracker, dr
 
                 if not dry_run:
                     for item, _ in marked_for_deletion:
-                        delete_setup: DeleteSetup = (item, lambda to_delete: manager.delete(item=to_delete))
+                        delete_setup: DeleteSetup = (item, _create_delete(manager))
                         all_area_delete_setups.append(delete_setup)
                 all_area_marked_for_deletion += marked_for_deletion
                 all_area_not_marked_for_deletion += not_marked_for_deletion
 
-            cleanup_areas_proposals[manager_type] = all_area_delete_setups, all_area_marked_for_deletion, \
+            cleanup_areas_plans[manager_type] = all_area_delete_setups, all_area_marked_for_deletion, \
                                                     all_area_not_marked_for_deletion
 
-        cleaup_proposal.append(cleanup_areas_proposals)
+        cleaup_plans.append(cleanup_areas_plans)
 
-    return cleaup_proposal
+    return cleaup_plans
 
 
-def create_human_explaination(proposals) -> str:
+def execute_plans(plans: CleanUpPlans, max_simultaneous_deletes: int):
     """
     TODO
-    :param proposals: 
+    :param plans: 
+    :param max_simultaneous_deletes: 
+    :return: 
+    """
+    all_delete_setups: List[DeleteSetup] = []
+    for plan in plans:
+        for _, (delete_setups, _, _) in plan.items():
+            all_delete_setups += delete_setups
+
+    with ThreadPoolExecutor(max_simultaneous_deletes) as executor:
+        for item, deleter in all_delete_setups:
+            _logger.info(f"Deleting: {create_human_identifier(item)}")
+            executor.submit(deleter, item)
+    _logger.info("All deleted")
+
+
+def create_human_explanation(plans: CleanUpPlans) -> str:
+    """
+    TODO
+    :param plans: 
     :return: 
     """
     lines: List[str] = []
-    for i in range(len(proposals)):
+    for i in range(len(plans)):
         lines.append(f"In cleanup configuration number {i +1}:")
-        proposal = proposals[i]
+        proposal = plans[i]
 
         for manager_type, (delete_setups, marked_for_deletion, not_marked_for_deletion) in proposal.items():
             for item, reasons in marked_for_deletion:
@@ -119,3 +143,20 @@ def _create_area_report(manager: Manager, prevent_delete_detectors: Iterable[Pre
             marked_for_deletion.append((item, to_delete_reasons))
 
     return marked_for_deletion, not_marked_for_deletion
+
+
+def _create_delete(manager: Manager) -> Callable[[OpenstackItem], None]:
+    """
+    TODO
+    :param manager: 
+    :param to_delete: 
+    :return: 
+    """
+    def delete(to_delete: OpenstackItem):
+        try:
+            assert manager.item_type == type(to_delete)
+            manager.delete(item=to_delete)
+        except Exception as e:
+            _logger.error(e)
+            raise
+    return delete
